4ee32272dc846998093c8f98e5b5256abcadd1bb
Tisankan <64467216+Tisankan@users.noreply.github.com>
2024-05-19 01:55:08 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const fs = require('fs');
 const app = express();
-const PORT = 3000;
+const PORT = 4000;
 
 // Function to read quotes from the JSON file
 function loadQuotes() {
